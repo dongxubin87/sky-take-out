78c2a49b4c1f5893edc3c3b29575a729cae80750
dongxubin87 <dongxubin87@gmail.com>
2025-10-15 11:06:51 +0300

    add new employee

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -15,4 +17,13 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    /**
+     * insert employee
+     * @param employee
+     */
+    @Insert("INSERT INTO employee " +
+            "(name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user,status) " +
+            "VALUES " +
+            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{status})")
+    void insert( Employee employee);
 }
